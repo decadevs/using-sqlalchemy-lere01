6a83462c70ab5b911279556b957700e4756982eb
Faith Oyedemi <faithoyedemi@yahoo.com>
2020-01-21 14:49:04 +0100

    make adjustment to parameters

--- Module/InMemoryStorage.py
+++ Module/InMemoryStorage.py
@@ -5,10 +5,10 @@ class InMemoryStorage(Interface):
     def __init__(self, list_of_books = {}):
         self.books = list_of_books
 
-    def create(self, Author = "", Title = ""):
+    def create(self, book_id = "", Author = "", Title = ""):
         record = {}
         record_id = len(self.books) + 1
-        record['id'] = record_id
+        record['id'] = book_id
         record['Author'] = Author
         record['Title'] = Title
         self.books[record_id] = record
@@ -17,9 +17,13 @@ class InMemoryStorage(Interface):
     def all(self):
         return self.books
 
-    def fetch(self, id = None, Author = "", Title = ""):
-        if id != None:
-            return self.books.get(id)
+    def fetch(self, book_id = None, Author = "", Title = ""):
+        if book_id != None:
+            book = self.books
+
+            for id in book:
+                if book[id]['id'] == book_id:
+                    return book.get(id)
 
         elif Author != "":
             book = self.books
@@ -37,9 +41,9 @@ class InMemoryStorage(Interface):
                     return book.get(id)
 
 
-    def delete(self, id = None):
+    def delete(self, book_id = None):
         try:
-            del self.books[id]
+            del self.books[book_id]
 
         except:
             raise Exception('make sure the identity is correct')
