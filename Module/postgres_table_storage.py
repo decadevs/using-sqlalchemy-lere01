d1f48c0184fd73421bd5228942d27ce52c732671
Faith Oyedemi <faithoyedemi@yahoo.com>
2020-01-21 19:58:23 +0100

    fix bugs

--- Module/postgres_table_storage.py
+++ Module/postgres_table_storage.py
@@ -1,6 +1,6 @@
 import sqlite3
-from .Interface import Interface
-from .create_database_table import Book
+from Module.Interface import Interface
+from Module.create_database_table import Book
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -18,22 +18,24 @@ class PostgresTableStorage(Interface):
         self.session = Session()
 
 
-    def create(self, book_id = "", author = "", title = ""):
+    def create(self, book_id = "", Author = "", Title = ""):
         # create object of the class Book
-        new_record = Book(book_id = book_id, author = author, title = title)
+        new_record = Book(book_id = book_id, author = Author, title = Title)
         session = self.session
         
         # add the book to the table
         session.add(new_record)
 
         # flush the transaction
-        session.commit(new_record)
+        session.commit()
 
-        return 'Record {} sucessfully created'.format(record_id)
+        return 'Record sucessfully created'
 
 
     def all(self):
         session = self.session
+        record = {}
+
         # create a Query object
         db_query = session.query(Book)
 
@@ -41,38 +43,65 @@ class PostgresTableStorage(Interface):
         db_rows = db_query.all()
         
         # return result object
-        return db_rows
+        for row in db_rows:
+            reco = {}
+            record_id = row.record_id
+            reco['id'] = row.book_id
+            reco['Author'] = row.author
+            reco['Title'] = row.title
+            record[row.record_id] = reco
+
+        return record
+
 
 
     def fetch(self, book_id = None, Author = "", Title = ""):
         db_query = self.session.query(Book)
+        record = {}
         
         if book_id != None:
             db_rows = db_query.filter(Book.book_id == book_id)
-            return db_rows
+            
+            for row in db_rows:
+                record['id'] = row.book_id
+                record['Author'] = row.author
+                record['Title'] = row.title
+            
+            return record
             
 
         elif Author != "":
             db_rows = db_query.filter(Book.author == Author)
-            return db_rows
+            
+            for row in db_rows:
+                record = {}
+
+                record['id'] = row.book_id
+                record['Author'] = row.author
+                record['Title'] = row.title
+            
+            return record
 
 
         elif Title != "":
             db_rows = db_query.filter(Book.title == Title)
+            
+            for row in db_rows:
+                record = {}
+
+                record['id'] = row.book_id
+                record['Author'] = row.author
+                record['Title'] = row.title
+            
+            return record
 
 
     def delete(self, book_id = None):
         db_query = self.session.query(Book)
-        delete_item = db_query.filter(Book.book_id == book_id)
-        pri_key = 0
-
-        for row in delete_item:
-            pri_key = row.record_id
-
-        self.session.delete(db_query.get(pri_key))
+        db_query.filter(Book.book_id == book_id).delete()
+        self.session.commit()
 
     
 
 
-    
 
