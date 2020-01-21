b51892c95cdfe745c5ffe4bf4d700c1a5e10875f
Faith Oyedemi <faithoyedemi@yahoo.com>
2020-01-21 13:20:16 +0100

    update variable names

--- test.py
+++ test.py
@@ -6,8 +6,8 @@ from Module.PostgresTableStorage import PostgresTableStorage
 class TestClass(unittest.TestCase):
 
     def setUp(self):
-        InMemoryStorage().create(id = 1, author = "Aka", title = "Python Introduction")
-        PostgresTableStorage().create(id = 2, author = "Abdulfatai", title = "Using SQLAlchemy")
+        InMemoryStorage().create(id = 1, Author = "Aka", Title = "Python Introduction")
+        PostgresTableStorage().create(id = 2, Author = "Abdulfatai", Title = "Using SQLAlchemy")
         return super().setUp()
 
     def tearDown(self):
@@ -18,19 +18,19 @@ class TestClass(unittest.TestCase):
     def test_InMemoryStorage(self):
         in_memomry = BooksManager(InMemoryStorage())
 
-        self.assertEqual(in_memomry.fetch(id = 1), 1)
-        self.assertDictEqual(in_memomry.fetch(author = "Aka"), {id: 1, author: "Aka", title: "Python Introduction"})
-        self.assertDictEqual(in_memomry.fetch(title = "Ruby", author = "Aka"), {id: 1, author: "Aka", title: "Python Introduction"})
-        self.assertListEqual(in_memomry.all(), [{id: 1, author: "Aka", title: "Python Introduction"}, {id: 2, author: "Abdulfatai", title: "Using SQLAlchemy"}])
+        self.assertEqual(in_memomry.fetch(id = 1), {id: 1, Author: "Aka", title: "Python Introduction"})
+        self.assertDictEqual(in_memomry.fetch(Author = "Aka"), {id: 1, Author: "Aka", title: "Python Introduction"})
+        self.assertDictEqual(in_memomry.fetch(Title = "Ruby", Author = "Aka"), {id: 1, Author: "Aka", Title: "Python Introduction"})
+        self.assertListEqual(in_memomry.all(), {1: {id: 1, Author: "Aka", Title: "Python Introduction"}, 2: {id: 2, Author: "Abdulfatai", Title: "Using SQLAlchemy"}})
 
 
     def test_PostgresTableStorage(self):
         in_table = BooksManager(PostgresTableStorage())
 
         self.assertEqual(in_table.fetch(id = 2), 2)
-        self.assertDictEqual(in_table.fetch(author = "Abdulfatai"), {id: 2, author: "Abdulfatai", title: "Using SQLAlchemy"})
-        self.assertDIctEqual(in_table.fetch(title = "Using SQLAlchemy", author = "Abdulfatai"), {id: 2, author: "Abdulfatai", title: "Using SQLAlchemy"})
-        self.assertListEqual(in_table.all(), [{id: 1, author: "Aka", title: "Python Introduction"}, {id: 2, author: "Abdulfatai", title: "Using SQLAlchemy"}])
+        self.assertDictEqual(in_table.fetch(Author = "Abdulfatai"), {id: 2, Author: "Abdulfatai", Title: "Using SQLAlchemy"})
+        self.assertDIctEqual(in_table.fetch(Title = "Using SQLAlchemy", Author = "Abdulfatai"), {id: 2, Author: "Abdulfatai", Title: "Using SQLAlchemy"})
+        self.assertListEqual(in_table.all(), {1: {id: 1, Author: "Aka", Title: "Python Introduction"}, 2: {id: 2, Author: "Abdulfatai", Title: "Using SQLAlchemy"}})
 
 
 
